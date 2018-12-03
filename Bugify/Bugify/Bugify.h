5d5b030d04a3612feb5b6f8136383e491624d749
Lukas Böhler <lukas@boehlerbrothers.com>
2018-12-03 13:39:25 +0100

    Init.

---
+++ Bugify/Bugify/Bugify.h
@@ -0,0 +1,19 @@
+//
+//  Bugify.h
+//  Bugify
+//
+//  Created by Lukas on 03.12.18.
+//  Copyright © 2018 Lukas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Bugify.
+FOUNDATION_EXPORT double BugifyVersionNumber;
+
+//! Project version string for Bugify.
+FOUNDATION_EXPORT const unsigned char BugifyVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Bugify/PublicHeader.h>
+
+
---
+++ Bugify/Bugify/Bugify.swift
@@ -0,0 +1,17 @@
+//
+//  Bugify.swift
+//  Bugify
+//
+//  Created by Lukas on 03.12.18.
+//  Copyright © 2018 Lukas. All rights reserved.
+//
+
+import Foundation
+
+public class Bugify {
+    static let shared = Bugify()
+    
+    func initialize(sdk: String) {
+        print("asdf" + sdk)
+    }
+}

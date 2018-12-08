7244602cf4cecba4b79ba1940cf65dc1addb05c7
Lukas Böhler <lukas@boehlerbrothers.com>
2018-12-08 11:16:52 +0100

    Add.

--- BugifyDemo/BugifyDemo/BugifyDemo/Bugify.swift
+++ BugifyDemo/BugifyDemo/BugifyDemo/Bugify.swift
@@ -14,6 +14,7 @@ class Bugify {
     static var apiKey = ""
     
     private var currentlyEditing = false
+    private var currentBugifyReportComposer: BugifyReportComposer?
     
     init(apiKey: String) {
         Bugify.apiKey = apiKey
@@ -32,14 +33,52 @@ class Bugify {
     }
     
     func showDebugReport(topController: UIViewController, callStack: [String]) {
-        let bugifyReportComposerViewController = UIStoryboard(name: "BugifyStoryboard", bundle: nil).instantiateViewController(withIdentifier: "BugifyReportComposer") as! BugifyReportComposer
+        self.currentBugifyReportComposer = UIStoryboard(name: "BugifyStoryboard", bundle: nil).instantiateViewController(withIdentifier: "BugifyReportComposer") as! BugifyReportComposer
         
         // Capture & populate screenshot.
         let screenShot = captureScreen(view: topController.view)
         
-        topController.present(bugifyReportComposerViewController, animated: true, completion: {
-            bugifyReportComposerViewController.screenShotImageView.image = UIImage(cgImage: screenShot!.cgImage!)
+        let navController = UINavigationController(rootViewController: self.currentBugifyReportComposer!)
+        self.currentBugifyReportComposer?.navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Send", style: .done, target: self, action: #selector(Bugify.sendFeedbackReport(sender:)))
+        self.currentBugifyReportComposer?.title = "Report a problem"
+        
+        topController.present(navController, animated: true, completion: {
+            self.currentBugifyReportComposer?.screenShotImageView.image = UIImage(cgImage: screenShot!.cgImage!)
+            self.currentBugifyReportComposer?.screenShotImageView.layer.addShadow()
+            self.currentBugifyReportComposer?.bugDescription.textContainerInset = UIEdgeInsets(top: 16, left: 12, bottom: 16, right: 12)
+            NotificationCenter.default.addObserver(self, selector: #selector(self.keyboardWillShow), name: UIResponder.keyboardWillShowNotification, object: nil)
+            
+            let defaults = UserDefaults.standard
+            self.currentBugifyReportComposer?.senderEmail.text = defaults.string(forKey: "bugifyDefaultSenderEmail") ?? ""
+            
+            self.currentBugifyReportComposer?.bugDescription.text = "Describe your problem..."
+            self.currentBugifyReportComposer?.bugDescription.textColor = UIColor.lightGray
+            self.currentBugifyReportComposer?.bugDescription.delegate = self.currentBugifyReportComposer
         })
+    }
+    
+    @objc func keyboardWillShow(_ notification: Notification) {
+        if let keyboardFrame: NSValue = notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue {
+            let keyboardRectangle = keyboardFrame.cgRectValue
+            let keyboardHeight = keyboardRectangle.height - 16
+            self.currentBugifyReportComposer?.bugDescriptionBottom.constant = (keyboardHeight - 16) * -1
+            self.currentBugifyReportComposer?.screenShotImageViewBottom.constant = keyboardHeight
+        }
+    }
+    
+    @objc func sendFeedbackReport(sender: UIBarButtonItem) {
+        // Save sender
+        let defaults = UserDefaults.standard
+        defaults.set(self.currentBugifyReportComposer?.senderEmail.text, forKey: "bugifyDefaultSenderEmail")
+        
+        // Send to server
+        let url = URL(string: "http://www.stackoverflow.com")!
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        NSURLConnection.sendAsynchronousRequest(request, queue: OperationQueue.main) {(response, data, error) in
+            guard let data = data else { return }
+            print(String(data: data, encoding: .utf8)!)
+        }
     }
     
     func getTopController() -> UIViewController? {
@@ -61,9 +100,38 @@ class Bugify {
     }
 }
 
-class BugifyReportComposer: UIViewController {
+extension CALayer {
+    func addShadow() {
+        self.shadowOffset = .zero
+        self.shadowOpacity = 0.15
+        self.shadowRadius = 6
+        self.shadowColor = UIColor.black.cgColor
+        self.masksToBounds = false
+        self.borderColor = UIColor.init(red: 0.0, green: 0.0, blue: 0.0, alpha: 0.2).cgColor
+        self.borderWidth = 0.5
+    }
+}
+
+class BugifyReportComposer: UIViewController, UITextViewDelegate {
     @IBOutlet weak var screenShotImageView: UIImageView!
+    @IBOutlet weak var senderEmail: UITextField!
+    @IBOutlet weak var bugDescription: UITextView!
+    @IBOutlet weak var bugDescriptionBottom: NSLayoutConstraint!
+    @IBOutlet weak var screenShotImageViewBottom: NSLayoutConstraint!
     
+    func textViewDidBeginEditing(_ textView: UITextView) {
+        if textView.textColor == UIColor.lightGray {
+            textView.text = nil
+            textView.textColor = UIColor.black
+        }
+    }
+    
+    func textViewDidEndEditing(_ textView: UITextView) {
+        if textView.text.isEmpty {
+            textView.text = "Describe your problem..."
+            textView.textColor = UIColor.lightGray
+        }
+    }
 }
 
 extension UIWindow {
